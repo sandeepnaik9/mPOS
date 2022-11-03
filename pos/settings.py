baca343b9eea9f158ec1c808a705e06a156c2454
mahendar r <sandeepnaik9900@gmail.com>
2022-11-03 22:01:34 +0530

    Static

--- pos/settings.py
+++ pos/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (
     # location of your application, should not be public web accessible 
     './static',
